3f7cffa44e94257978851b22ba54c1621919e46b
aluno <vinicius.s.silveira.2015@gmail.com>
2024-03-14 19:26:03 -0300

    Novo17

---
+++ src/main/java/com/mycompany/contagemregressiva/Contagemregressiva.java
@@ -0,0 +1,16 @@
+package com.mycompany.contagemregressiva;
+
+import java.util.Scanner;
+
+public class Contagemregressiva {
+
+public static void main(String[] args) {
+
+Scanner teclado = new Scanner (System.in);
+
+
+for (int i=8; i>=0; i--){
+    System.out.println("Valor de i:" + i);
+}
+    }
+}
